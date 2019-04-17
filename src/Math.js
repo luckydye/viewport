839258807f9d7ffb96dcf22766694791f55faa40
Tim Havlicek <tim.havlicek@chamaeleon.de>
2019-04-17 11:00:12 +0200

    ray cast

--- src/Math.js
+++ src/Math.js
@@ -1,48 +1,56 @@
+import { mat4 } from "gl-matrix";
+
 export class Vec extends Array {
 
 	static avg(vec1, vec2) {
 		return new Vec(
-			(vec2.x + vec1.x) / 2,
-			(vec2.y + vec1.y) / 2,
-			(vec2.z + vec1.z) / 2,
+			(vec2[0] + vec1[0]) / 2,
+			(vec2[1] + vec1[1]) / 2,
+			(vec2[2] + vec1[2]) / 2,
 		);
 	}
 
 	static add(vec1, vec2) {
 		return new Vec(
-			vec1.x + vec2.x,
-			vec1.y + vec2.y,
-			vec1.z + vec2.z
+			vec1[0] + vec2[0],
+			vec1[1] + vec2[1],
+			vec1[2] + vec2[2],
+			vec1[3] + vec2[3]
 		);
 	}
 
 	static multiply(vec1, vec2) {
 		return new Vec(
-			vec1.x * vec2.x,
-			vec1.y * vec2.y,
-			vec1.z * vec2.z
+			vec1[0] * vec2[0],
+			vec1[1] * vec2[1],
+			vec1[2] * vec2[2],
+			vec1[3] * vec2[3],
 		);
 	}
 
 	get x() { return this[0]; }
 	get y() { return this[1]; }
 	get z() { return this[2]; }
+	get w() { return this[3]; }
 
 	set x(val) { this[0] = val; }
 	set y(val) { this[1] = val; }
 	set z(val) { this[2] = val; }
+	set w(val) { this[3] = val; }
 
-	constructor(x = 0, y = 0, z = 0) {
+	constructor(x = 0, y = 0, z = 0, w = 0) {
 		super();
 
 		this[0] = x;
 		this[1] = y;
 		this[2] = z;
+		this[3] = w;
 
 		if(arguments.length === 1) {
 			this[0] = x[0];
 			this[1] = x[1];
 			this[2] = x[2];
+			this[3] = x[3];
 		}
 	}
 
@@ -53,6 +61,24 @@ export class Vec extends Array {
 	multiply(vec) {
 		return Vec.multiply(this, vec);
 	} 
+}
+
+export class Ray extends Vec {
+
+	setPosition(vx, vy, vwidth, vheight) {
+		this[0] = (2 * vx) / vwidth - 1;
+		this[1] = 1 - (2 * vy) / vheight;
+		this[2] = -1;
+		this[3] = 1;
+	}
+
+	cast(projMatrix) {
+		// ec4 ray_eye = inverse(projection_matrix) * ray_clip;
+		// ray_eye = vec4(ray_eye.xy, -1.0, 0.0);
+
+		mat4.invert(projMatrix) * ray_eye;
+	}
+
 }
 
 export class Transform {
