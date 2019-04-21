88fe782bfa89619827590434e755eb73f234246b
Tim Havlicek <tim.h4vlicek@gmail.com>
2019-04-21 12:50:04 +0200

    animation for test

--- test/model/main.js
+++ test/model/main.js
@@ -3,6 +3,7 @@ import { Transform, Vec } from "../../src/Math.js";
 import Config from "../../src/Config.js";
 import { Resources } from "../../src/Resources.js";
 import { Loader } from "../../src/Loader.js";
+import { Keyframe, Animation } from "../../src/Animation.js";
 
 const viewport = new Viewport();
 
@@ -19,6 +20,14 @@ viewport.onload = () => {
 
     const camera = viewport.camera;
 
+    const anim1 = new Animation();
+    anim1.setKeyframe(new Keyframe(new Vec(400, 800, 400)));
+    anim1.play(camera, 'position');
+
+    const anim2 = new Animation();
+    anim2.setKeyframe(new Keyframe(new Vec(400, 800, 400)));
+    anim2.play(camera, 'position');
+
     const savedPosition = Config.global.getValue('camera');
     if(savedPosition) {
         camera.setPositionTo(new Transform(savedPosition));
@@ -26,6 +35,10 @@ viewport.onload = () => {
     setInterval(() => {
         Config.global.setValue('camera', camera);
     }, 1000);
+
+    setInterval(() => {
+        campos.innerText = camera.position + "  |  " + camera.rotation;
+    }, 16);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
