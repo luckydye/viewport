4c791ae56418c9e578e15d2f8f3f1d913de4a57b
Tim Havlicek <tim.h4vlicek@gmail.com>
2019-06-15 19:25:49 +0200

    construct res path at load

--- src/Resources.js
+++ src/Resources.js
@@ -14,7 +14,7 @@ global.resourceTypes = {
 
 global.queue = new Set();
 global.map = new Map();
-global.resourceRoot = '../res/';
+global.resourceRoot = './res/';
 
 /*
 	Resource.add({ name, path }: arr, startLoading: bool): startLoading ? Promise : null
@@ -53,7 +53,7 @@ export class Resources {
 
 	static add(obj, startLoad) {
 		for(let key in obj) {
-			global.queue.add({ name: key, path: global.resourceRoot + obj[key] });
+			global.queue.add({ name: key, path: obj[key] });
 		}
 		if(startLoad === true) {
 			return Resources.load();
@@ -72,7 +72,7 @@ export class Resources {
 		let loads = [];
 
 		for(let res of global.queue) {
-			const loading = Resources._fetch(res.path).then(dataObj => {
+			const loading = Resources._fetch(global.resourceRoot + '/' + res.path).then(dataObj => {
 				const resource = res;
 				global.map.set(resource.name, dataObj);
 			});
