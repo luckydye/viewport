79cfd8ca4486e8f13a4f0244151c8d2619072097
Tim Havlicek <tim.h4vlicek@gmail.com>
2019-09-01 19:24:04 +0200

    free player controller with velocity

--- src/controlers/CameraControler.js
+++ src/controlers/CameraControler.js
@@ -10,14 +10,14 @@ export class CameraControler extends EntityControler {
 		const entity = this.entity;
 
 		const down = e => {
-			if(EntityControler.isMouseButton(e) == 2) {
+			if (EntityControler.isMouseButton(e) == 2) {
 				this.unlock();
 				this.viewport.requestPointerLock();
 			}
 		}
 
 		const move = e => {
-			if(document.pointerLockElement != null) {
+			if (document.pointerLockElement != null) {
 				entity.rotation.y += e.movementX * this.sensivity;
 				entity.rotation.x += e.movementY * this.sensivity;
 
@@ -31,34 +31,34 @@ export class CameraControler extends EntityControler {
 		this.viewport.addEventListener("mousedown", down);
 		this.viewport.addEventListener("mousemove", move);
 	}
-	
+
 	up() {
 		const camera = this.entity;
-		camera.position.y -= this.speed;
+		camera.position.y -= this.speed * 2;
 	}
 
 	down() {
 		const camera = this.entity;
-		camera.position.y += this.speed;
+		camera.position.y += this.speed * 2;
 	}
 
 	move(dir) {
 		const camera = this.entity;
 		const speed = this.speed * dir;
-		
+
 		const a = -camera.rotation.y;
 		const b = -camera.rotation.x;
 
 		camera.position.x += speed * Math.sin(a);
 		camera.position.z += speed * Math.cos(a);
-		
+
 		camera.position.y -= speed * Math.sin(b);
 	}
 
 	strafe(dir) {
 		const camera = this.entity;
 		const speed = this.speed * dir;
-		
+
 		const a = camera.rotation.y;
 
 		camera.position.z += speed * Math.sin(a);
@@ -66,14 +66,14 @@ export class CameraControler extends EntityControler {
 	}
 
 	update() {
-		if(this.checkKey("w")) this.move(1);
-		if(this.checkKey("s")) this.move(-1);
+		if (this.checkKey("w")) this.move(1);
+		if (this.checkKey("s")) this.move(-1);
 
-		if(this.checkKey("a")) this.strafe(1);
-		if(this.checkKey("d")) this.strafe(-1);
+		if (this.checkKey("a")) this.strafe(1);
+		if (this.checkKey("d")) this.strafe(-1);
 
-		if(this.checkKey("q")) this.up();
-		if(this.checkKey("y")) this.down();
+		if (this.checkKey("q")) this.up();
+		if (this.checkKey("y")) this.down();
 	}
 
 	lock() {
--- src/controlers/PlayerControler.js
+++ src/controlers/PlayerControler.js
@@ -9,87 +9,61 @@ export class PlayerControler extends CameraControler {
 		this.sensivity = 0.0025;
 		this.speed = 5;
 		this.maxSpeed = 1;
-		this.jumpSpeed = 10;
+		this.weight = 133;
 		this.direction = new Vec();
 	}
 
-	jump(dt) {
-		if (!this.entity.airborn) {
-			this.entity.velocity.y -= this.jumpSpeed;
-		}
-		this.entity.airborn = true;
+	move(dir) {
+		this.direction.z = dir;
 	}
 
-	move(dir) {
-		this.direction.x = dir * this.maxSpeed;
-		let speed = this.speed * this.direction.x;
-
-		if (this.entity.airborn) {
-			speed *= 0.2;
-		}
-
-		const camera = this.entity;
-		const a = -camera.rotation.y;
-
-		if (this.direction.z) {
-			speed = speed * 1;
-		}
-
-		this.entity.velocity.x += speed * Math.sin(a);
-		this.entity.velocity.z += speed * Math.cos(a);
+	pan(dir) {
+		this.direction.y = -dir;
 	}
 
 	strafe(dir) {
-		this.direction.z = dir * this.maxSpeed;
-		let speed = this.speed * this.direction.z;
-
-		if (this.entity.airborn) {
-			speed *= 0.2;
-		}
-
-		const camera = this.entity;
-		const a = camera.rotation.y;
-
-		if (this.direction.x) {
-			speed = speed * 1;
-		}
-
-		this.entity.velocity.x += speed * Math.cos(a);
-		this.entity.velocity.z += speed * Math.sin(a);
+		this.direction.x = dir;
 	}
 
 	update(dt) {
 		const entity = this.entity;
 
-		if (entity.velocity) {
-			entity.velocity.y += 0.45;
-			entity.position.y += entity.velocity.y;
+		if (this.checkKey("w")) this.move(this.speed);
+		if (this.checkKey("s")) this.move(-this.speed);
 
-			if (entity.position.y > -entity.height) {
-				entity.position.y = -entity.height;
-				entity.velocity.y = 0;
-				entity.airborn = false;
-			}
-		}
+		if (this.checkKey("a")) this.strafe(this.speed);
+		if (this.checkKey("d")) this.strafe(-this.speed);
 
-		if (this.checkKey("w")) this.move(1);
-		if (this.checkKey("s")) this.move(-1);
+		if (this.checkKey("q")) this.pan(this.speed);
+		if (this.checkKey("y")) this.pan(-this.speed);
 
-		if (this.checkKey("a")) this.strafe(1);
-		if (this.checkKey("d")) this.strafe(-1);
+		const a = -this.entity.rotation.y;
+		const b = -this.entity.rotation.x;
 
-		if (this.checkKey(" ")) this.jump();
+		const camDirectionInv = new Vec(
+			Math.sin(-this.entity.rotation.y),
+			Math.max(Math.min(Math.tan(this.entity.rotation.x), 1), -1),
+			Math.cos(-this.entity.rotation.y),
+		)
+
+		const camDirection = new Vec(
+			Math.sin(this.entity.rotation.y),
+			Math.max(Math.min(Math.tan(this.entity.rotation.x), 1), -1),
+			Math.cos(this.entity.rotation.y),
+		)
+
+		this.entity.velocity.x += (this.direction.z * camDirectionInv.x) + (this.direction.x * camDirection.z);
+		this.entity.velocity.y += (this.direction.z * camDirectionInv.y) + this.direction.y;
+		this.entity.velocity.z += (this.direction.z * camDirectionInv.z) + (this.direction.x * camDirection.x);
 
 		this.entity.position.x += this.entity.velocity.x;
+		this.entity.position.y += this.entity.velocity.y;
 		this.entity.position.z += this.entity.velocity.z;
 
-		let resistance = 0.8;
-
-		if (this.entity.airborn) {
-			resistance = 0.94;
-		}
+		let resistance = 100 / this.weight;
 
 		this.entity.velocity.x *= resistance;
+		this.entity.velocity.y *= resistance;
 		this.entity.velocity.z *= resistance;
 
 		this.direction.x = 0;
