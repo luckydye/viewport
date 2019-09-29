03f6908e5bed552bc7a025a8beffac989f4ad44c
Tim Havlicek <tim.h4vlicek@gmail.com>
2019-09-29 03:17:04 +0200

    shadow bias

--- src/shader/DefaultShader.js
+++ src/shader/DefaultShader.js
@@ -93,7 +93,7 @@ export default class DefaultShader extends MeshShader {
 
                 float shadowmap_distance = shadowmap_color.r;
 
-                if (vertex_relative_to_light.z <= shadowmap_distance + 0.0002) {
+                if (vertex_relative_to_light.z <= shadowmap_distance + 0.00035) {
                     finalColor.rgb *= 1.0;
                 } else {
                     finalColor.rgb *= 1.0 - shadowColor.a;
